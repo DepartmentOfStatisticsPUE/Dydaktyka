a9465ecc9956596a4f36c6cffe984d0045cad243
Maciej Beręsewicz <maciej.beresewicz@gmail.com>
2016-10-28 17:55:06 +0200

    schemat losowania i nowe dane

--- Metoda reprezentacyjna/2016-2017/codes/01-przygotowanie-danych.R
+++ Metoda reprezentacyjna/2016-2017/codes/01-przygotowanie-danych.R
@@ -13,7 +13,7 @@ load('~/Documents/Uczelnia/Papers/In_preparation/Dagmara/datasets/bkl_subset.rda
 
 bkl_2010_2014 <- bkl_lud %>% 
   filter(edycja %in% c(2010, 2014)) %>%
-  select(respid:eduk_lata) %>%
+  select(respid:eduk_lata,n0) %>%
   mutate(jps = paste(powiat, sGUS, paste = '_')) %>%
   left_join(bkl_subset %>% 
               select(respid,kodpod)) %>%
@@ -27,7 +27,7 @@ save(bkl_2010_2014,file = 'Metoda reprezentacyjna/2016-2017/data/bkl_2010_2014.R
 
 
 slownik <- sjPlot::view_df(
-  x = bkl_2010_2014,
+  x = bkl_2010_2014[,-1],
   show.values = F,
   show.frq = T,
   show.na  = T,
@@ -42,8 +42,6 @@ slownik_df <- xml2::read_html('Metoda reprezentacyjna/2016-2017/documents/bkl_sl
   rvest::html_table(header = T,
                     fill = T)
 
-tbl_df(slownik_df[[1]])
-
 slownik_df[[1]] %>%
   select(ID:missings) %>%
   rename(nazwa_zmiennej = Name, etykieta = Label, braki_danych = missings) %>%
@@ -56,10 +54,20 @@ library(survey)
 design <- svydesign(
   ids = ~sGUS,
   strata = ~edycja + wojew + kodpod + miejsce + m1_plec + wiek_4k,
-  data = bkl_2010_2014,
+  data = data.frame(bkl_2010_2014 %>% 
+                      filter(!is.na(wiek_4k),!is.na(n0))),
   weights = ~waga_pop,
   nest = T
 )
 
+svytable(~n0,design = design)
+
+df <- svyby(
+  formula = ~factor(n0),
+  by = ~ edycja + kodpod,
+  design = design,
+  FUN = svymean
+) 
+df
 
 
