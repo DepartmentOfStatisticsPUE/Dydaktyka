ed2b299dca160042199aa63a48d81ee55cb7ee63
Maciej Beręsewicz <maciej.beresewicz@gmail.com>
2016-11-08 17:01:34 +0100

    zbior danych tabela do html

--- Wizualizacja i Raportowanie Danych/2016-2017/codes/2016-11-08.R
+++ Wizualizacja i Raportowanie Danych/2016-2017/codes/2016-11-08.R
@@ -1,18 +1,56 @@
 ### Zajęcia 8.11.2016
 
+library('htmlTable')
+library('knitr')
+library('rmarkdown')
+
+dane <- data.frame(
+  x = sample(1:2, size = 10, rep = T),
+  y = sample(1:2, size = 10, rep = T),
+  z = sample(1:2, size = 10, rep = T))
+
+htmlTable(dane)
+
+## wskazuję gdzie zapisać
+sink(file = 'tabela.html') 
+### co ma być zapisane w pliku
+kable(x = dane,
+      format = 'html')
+## koniec zapisywania
+sink()
+
+#### 
+
+
+
+
 ## Co będziemy robić na zajeciach?
 ### cel wykorzystania wykresów punktowych / rozrzutu
-### analiza 
+### analiza
 ### przetworzenie zbioru do analizy wielowymiarowej z wykorzystaniem facet_wrap
 
-library(dplyr)
-library(rio)
+library(PBImisc)
+library(ggplot2)
 
-### wczytujemy dane o studentach
-stu <- import('Wizualizacja i Raportowanie Danych/2016-2017/datasets/BKL_studenci_1_4ed.sav')
+data(apartments)
 
-tbl_df(stu) %>%
-  select(edycja,kod_A1_1min,A6,starts_with('E1'),waga_polaczona) %>%
-  View()
+head(apartments)
 
+ggplot(data = apartments,
+       aes(
+         x = surface,
+         y = transaction.price,
+         group = type,
+         colour = type
+       )) +
+  #geom_point() +
+  geom_smooth(method = 'loess', se = F) +
+  theme_bw()
 
+ggplot(data = apartments,
+       aes(x = floor,
+           y = transaction.price)) +
+  geom_point() +
+  geom_smooth(method = 'loess') +
+  theme_bw()  +
+  scale_y_log10()
